74179f56cd9e689f7aa40b385a58708028fd1177
iliaasefy <iliaasefy1383@gmail.com>
2023-07-29 18:26:58 +0330

    adding new projects

--- adad_zoj/Weekday/Program.cs
+++ adad_zoj/Weekday/Program.cs
@@ -1,1 +1,31 @@
-﻿
+﻿for (int i = 1; i <= 7; i++)
+{
+    if (i == 1)
+    {
+        Console.WriteLine("today is saturday!");
+    }
+    else if (i == 2)
+    {
+        Console.WriteLine("today is sunday!");
+    }
+    else if (i == 3)
+    {
+        Console.WriteLine("today is monday!");
+    }
+    else if (i == 4)
+    {
+        Console.WriteLine("today is tuesday!");
+    }
+    else if (i == 5)
+    {
+        Console.WriteLine("today is wednesday!");
+    }
+    else if (i == 6)
+    {
+        Console.WriteLine("today is thursday!");
+    }
+    else if (i == 7)
+    {
+        Console.WriteLine("today is friday!");
+    }
+}
